183d9250740521ac5233221bb94305a59ab46776
OBM9 <TheLeviathan12@hotmail.com>
2023-09-11 14:55:05 -0700

    commit

--- Decade.py
+++ Decade.py
@@ -3,6 +3,6 @@ age = int(input("How old are you?\n"))
 decade = age // 10
 years = age % 10
 
-print("You are " + str(decade) + " decade and " + str(years) + "old.")
+print("You are " + str(decade) + " decade and " + str(years) + " old.")
 
 
