7a340a7c95d4df45da220fabe0aadbea0561c5ed
OBM9 <TheLeviathan12@hotmail.com>
2023-09-12 07:11:22 -0700

    commit

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,11 +3,11 @@ user_choice = input('Do you want rock, paper, or scissors?')
 
 if computer_choice == user_choice:
   print('TIE')
-elif user_choice == 'rock' and computer_choice == 'scissors'
+elif user_choice == 'rock' and computer_choice == 'scissors':
   print('WIN')
-elif user_choice == 'paper' and computer_choice == 'rock'
+elif user_choice == 'paper' and computer_choice == 'rock':
   print('WIN')
-elif user_choice == 'scissors' and computer_choice == 'paper'
+elif user_choice == 'scissors' and computer_choice == 'paper':
   print('WIN')
 else:
   print('You lose, computer wins :)')
