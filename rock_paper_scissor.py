cefa0aa25cc1f3f67235eade145df2da5c5b1182
OBM9 <TheLeviathan12@hotmail.com>
2023-09-12 07:01:08 -0700

    commit

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,3 +3,5 @@ user_choice = input('Do you want rock, paper, or scissors?')
 
 if computer_choice == user_choice:
   print('TIE')
+elif user_choice == 'rock' and computer_choice == 'scissors'
+print('WIN')
