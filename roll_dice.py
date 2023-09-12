366e54bf39917e8c326d5e2e4d9335aa9fb164d8
OBM9 <TheLeviathan12@hotmail.com>
2023-09-12 11:09:22 -0700

    commit

--- roll_dice.py
+++ roll_dice.py
@@ -4,3 +4,5 @@ roll = random.randint(1,6)
 guess = int(input('Guess the dice roll:\n'))
 if guess == roll:
   print("Correct! They rolled a " + str(roll))
+else:
+  print("Wrong! They rolled a " + str(roll))
