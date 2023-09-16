4b204d323d03e55769ed50d75e05873487880925
OBM9 <TheLeviathan12@hotmail.com>
2023-09-16 01:50:44 -0700

    commit

--- loan.py
+++ loan.py
@@ -1,5 +1,5 @@
 money_owed = float(input('How much money do you owe, in dollars?\n '))
-apr = float(input('What is the annual percentage rate of the loan?'\n))
+apr = float(input('What is the annual percentage rate of the loan?\n'))
 payment = float(input('How much will you pay off each month in dollars?\n'))
 months = int(input('How many months do you want to see the results for?\n'))
 
