8756e9e71ef5704443cde9e1c226fe6a3c9ddff5
OBM9 <TheLeviathan12@hotmail.com>
2023-09-17 02:59:36 -0700

    commit

--- loan.py
+++ loan.py
@@ -3,7 +3,8 @@ apr = float(input('What is the annual percentage rate of the loan?\n'))
 payment = float(input('How much will you pay off each month in dollars?\n'))
 months = int(input('How many months do you want to see the results for?\n'))
 
-monthly_rate = apr/100/12  
+monthly_rate = apr/100/12 
+ 
 interest_paid = money_owed * monthly_rate
 money_owed = money_owed + interest_paid
 money_owed = money_owed - payment
